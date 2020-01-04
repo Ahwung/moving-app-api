38f9486f64602fb4ddc2f3a79a3cda0030e8b176
Adam Hwung <Ahwung@gmail.com>
2020-01-03 22:47:21 -0800

    changing stuff for heroku

--- controllers/products.php
+++ controllers/products.php
@@ -1,8 +1,13 @@
 <?php
 
-$dbconn = pg_connect(getenv("DATABASE_URL"));
+// $dbconn = pg_connect(getenv("DATABASE_URL"));
 
 include_once __DIR__ . '/../models/product.php';
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 
 if ($_REQUEST['action'] === 'index') {
     echo json_encode(Products::all());
