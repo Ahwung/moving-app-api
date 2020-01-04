f8b0c66a380f2973f49d9c85882c444841260e28
Adam Hwung <Ahwung@gmail.com>
2020-01-03 22:28:11 -0800

    changed seed file

--- seed.sql
+++ seed.sql
@@ -11,11 +11,11 @@ CREATE TABLE products(
     store VARCHAR(64),
     room VARCHAR(64),
     category VARCHAR(64),
-    bought BOOLEAN DEFAULT false
+    bought VARCHAR(64)
 );
 
-INSERT INTO products (name, price, url, store, room, category) VALUES ('Ibiza Sofa', 299.99, 'https://www.wayfair.com/furniture/pdp/zipcode-design-ibiza-sofa-zipc7699.html', 'Wayfair', 'Living Room', 'Furniture');
+INSERT INTO products (name, price, url, store, room, category, bought) VALUES ('Ibiza Sofa', 299.99, 'https://www.wayfair.com/furniture/pdp/zipcode-design-ibiza-sofa-zipc7699.html', 'Wayfair', 'Living Room', 'Furniture', 'False');
 
-INSERT INTO products (name, price, url, store, room, category) VALUES ('Gloucester Upholstered Standard Bed', 162.99, 'https://www.wayfair.com/furniture/pdp/greyleigh-gloucester-upholstered-standard-bed-w001553030.html', 'Wayfair', 'Bedroom', 'Furniture');
+INSERT INTO products (name, price, url, store, room, category, bought) VALUES ('Gloucester Upholstered Standard Bed', 162.99, 'https://www.wayfair.com/furniture/pdp/greyleigh-gloucester-upholstered-standard-bed-w001553030.html', 'Wayfair', 'Bedroom', 'Furniture', 'False');
 
-INSERT INTO products (name, price, url, store, room, category) VALUES ('Sonja 8 Drawer Double Dresser', 447.84, 'https://www.wayfair.com/furniture/pdp/sonja-8-drawer-double-dresser-lrfy4323.html', 'Wayfair', 'Bedroom', 'Furniture');
+INSERT INTO products (name, price, url, store, room, category, bought) VALUES ('Sonja 8 Drawer Double Dresser', 447.84, 'https://www.wayfair.com/furniture/pdp/sonja-8-drawer-double-dresser-lrfy4323.html', 'Wayfair', 'Bedroom', 'Furniture', 'True');
